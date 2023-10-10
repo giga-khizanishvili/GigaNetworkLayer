4ac6c7d6239f60ceffbe2e99b5696c15efc9fbc2
Giga Khizanishvili <giga.khizanishvili@space.ge>
2023-10-11 00:20:41 +0400

    Create GigaNetworkLayerShowroom

---
+++ GigaNetworkLayerShowroom/GigaNetworkLayerShowroom/GigaNetworkLayerShowroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigaNetworkLayerShowroomApp.swift
+//  GigaNetworkLayerShowroom
+//
+//  Created by Giga Khizanishvili on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct GigaNetworkLayerShowroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
